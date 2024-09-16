2d0613fd4dd048e2f7591b5e221e1cb8ba79255a
cliffamadeus <reconcliffy@gmail.com>
2024-09-16 14:21:17 +0800

    Added applet 1

---
+++ app.js
@@ -0,0 +1,6 @@
+//Event Function 1
+function updateDisplay() {
+    var textbox = document.getElementById('textbox');
+    var displayText = document.getElementById('displayText');
+    displayText.textContent = textbox.value;
+}
