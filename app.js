6d91ed2b730ceb256bdb1897bd02d8c7e150e9a9
cliffamadeus <reconcliffy@gmail.com>
2024-09-16 15:04:47 +0800

    Transferred inline scripts into app.js

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ function updateDisplay() {
     var displayText = document.getElementById('displayText');
     displayText.textContent = textbox.value;
 }
+
+//Event Function 2
+function showAlert() {
+    var textbox = document.getElementById('textbox');
+    var text = textbox.value;
+    alert('You entered: ' + text.value);
+}
