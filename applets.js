fe27a59858a2f2116e79889f3debf85bc1590f45
Cliffmeister <dev.clfm5t12@gmail.com>
2024-10-14 06:41:27 +0800

    Added search filter for applets

--- applets.js
+++ applets.js
@@ -20,25 +20,45 @@ class AppletCard {
 }
 
 class AppletRenderer {
-    constructor(containerId) {
+    constructor(containerId,searchInputId) {
         this.container = document.getElementById(containerId);
+
+        //
+        this.searchInput = document.getElementById(searchInputId);
+        this.appletData = [];
+        this.filteredData = [];
+        this.searchInput.addEventListener('input',()=> this.filterApplets());
     }
 
     fetchAppletData(url) {
         fetch(url)
             .then(response => response.json())
-            .then(data => this.renderApplets(data))
+            .then(data => {
+                this.appletData = data;
+                this.filteredData = data;
+                this.renderApplets(this.filteredData);
+            })
             .catch(error => console.error('Error loading applet data:', error));
     }
 
     renderApplets(data) {
+        this.container.innerHTML = '';
         data.forEach(applet => {
             const appletCard = new AppletCard(applet.title, applet.description, applet.link);
             const cardElement = appletCard.createCard();
             this.container.appendChild(cardElement);
         });
     }
+
+    filterApplets(){
+        const query = this.searchInput.value.toLowerCase();
+        this.filteredData = this.appletData.filter(applet =>
+            applet.title.toLowerCase().includes(query) ||
+            applet.description.toLowerCase().includes(query)
+        );
+        this.renderApplets(this.filteredData);
+    }
 }
 
-const appletRenderer = new AppletRenderer('applet-container');
+const appletRenderer = new AppletRenderer('applet-container','searchApplet');
 appletRenderer.fetchAppletData('applets.json');
