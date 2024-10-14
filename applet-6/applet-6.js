b5941b683d0d9152c4462ca40275e9fd22dee756
Cliffmeister <dev.clfm5t12@gmail.com>
2024-10-14 21:23:17 +0800

    Added disable buttons when clicked

--- applet-6/applet-6.js
+++ applet-6/applet-6.js
@@ -41,6 +41,9 @@ class TodoList {
         const taskItem = event.target.closest('.todo-item');
         const taskText = taskItem.querySelector('.task-text');
         taskText.classList.toggle('completed'); 
+
+        const buttons = taskItem.querySelectorAll('button');
+        buttons.forEach(button => button.disabled = true);
     }
 
     updateTask(taskText) {
